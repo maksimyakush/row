f76da73aa366a4989f38b79b42d989f562cbcf32
maksimyakush <maksimo-1988@yandex.ru>
2022-03-05 16:20:54 +0300

    commit

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const requestIp = require('request-ip');
 
 const app = express();
+app.use(requestIp.mw())
 app.post('/error', async(req, res) => {
-    console.log(req.clientIp);
+    console.log(`ip: ${req.ip}, clientIp: ${req.clientIp}`);
   res.header("Access-Control-Allow-Origin", "*");
   res.send('success');
 })
 
 app.get('/', async(req, res) => {
   console.log(req.ip);
-  res.send(req.ip);
+res.send(`ip: ${req.ip}, clientIp: ${req.clientIp}`);
 })
 
-app.listen(process.env.PORT || 3000, () => console.log('server works'))
+app.listen(3000, () => console.log('server works'))
